631f4953fae616ebd2d6437f087708de95a298ff
NOUR-OMAR <noora.mohammad14@gmail.com>
2021-08-26 15:44:47 +0300

    in memory db as web project

---
+++ src/test/java/InMemoryDB/InMemoryDB/InMemoryDbApplicationTests.java
@@ -0,0 +1,13 @@
+package InMemoryDB.InMemoryDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InMemoryDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
