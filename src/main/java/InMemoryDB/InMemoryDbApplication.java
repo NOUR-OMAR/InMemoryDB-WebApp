ec22d0bbaa89b417c44d8b20ef21edbfe81de7f5
NOUR-OMAR <noora.mohammad14@gmail.com>
2021-09-07 03:38:47 +0300

    update InMemoryDbApplication

--- src/main/java/InMemoryDB/InMemoryDbApplication.java
+++ src/main/java/InMemoryDB/InMemoryDbApplication.java
@@ -2,14 +2,21 @@ package InMemoryDB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication(scanBasePackages = {"InMemoryDB"})
 @EnableWebSecurity
-public class InMemoryDbApplication {
+public class InMemoryDbApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(InMemoryDbApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(InMemoryDbApplication.class);
     }
 
 }
