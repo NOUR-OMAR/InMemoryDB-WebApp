37970ba5c23205af2fbb65dc4f110914abea4c21
NOUR-OMAR <noora.mohammad14@gmail.com>
2021-09-08 06:07:56 +0300

    put files in resourses

--- src/main/java/InMemoryDB/utils/Constant.java
+++ src/main/java/InMemoryDB/utils/Constant.java
@@ -7,17 +7,17 @@ import java.io.File;
 
 public final class Constant {
 
-    static File employeesCSVFile = new File("employees.csv");
+    static File employeesCSVFile = new File("src/main/resources/employees.csv");
     public static final String EMPLOYEES_CSV_PATH = employeesCSVFile.getAbsolutePath();
-    static File departmentsCSVFile = new File("departments.csv");
+    static File departmentsCSVFile = new File("src/main/resources/departments.csv");
     public static final String DEPARTMENTS_CSV_PATH = departmentsCSVFile.getAbsolutePath();
-    static File usersCSVFile = new File("Users.csv");
+    static File usersCSVFile = new File("src/main/resources/Users.csv");
     public static final String USERS_FILE_PATH = usersCSVFile.getAbsolutePath();
-    static File employeesLoggerFile = new File("employees_logger.txt");
+    static File employeesLoggerFile = new File("src/main/resources/employees_logger.txt");
     public static final String EMPLOYEES_LOGGER_FILE = employeesLoggerFile.getAbsolutePath();
-    static File departmentsLoggerFile = new File("departments_logger.txt");
+    static File departmentsLoggerFile = new File("src/main/resources/departments_logger.txt");
     public static final String DEPARTMENTS_LOGGER_FILE = departmentsLoggerFile.getAbsolutePath();
-    static File usersLoggerFile = new File("users_logger.txt");
+    static File usersLoggerFile = new File("src/main/resources/users_logger.txt");
     public static final String USERS_LOGGER_FILE = usersLoggerFile.getAbsolutePath();
 
     public static final int CACHE_MAX_SIZE = 1000;
